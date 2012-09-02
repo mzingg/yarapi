78950dea65ad4bd7397032485eb16e6caad5410d
Markus Zingg <mzingg@gmx.net>
2012-09-02 20:11:04 +0200

    Better handling of the application context if it has the name /yarapi

--- src/core/modules/command/command.module.php
+++ src/core/modules/command/command.module.php
@@ -3,7 +3,7 @@
 function command_request(Task $oTask, $oResult) {
   // Only answer to /yarapi/command/<commandname>
   if ($oTask->getPathCount() < 3 || $oTask->getPathArg(0) != 'yarapi' || $oTask->getPathArg(1) != 'command') 
-	return;
+		return;
   
   $sCommand = $oTask->getPathArg(2);
   $aArguments = $oTask->getArguments();
@@ -12,7 +12,7 @@ function command_request(Task $oTask, $oResult) {
   $oResult->arguments = $aArguments; 
   $oResult->results = Modules::invokeHook('cmd_' . $sCommand, $aArguments);
   if (!$oResult->results)
-	$oResult->results = Modules::invokeHook('command', $sCommand, $aArguments);
+		$oResult->results = Modules::invokeHook('command', $sCommand, $aArguments);
   
   $oTask->setHandled();
 }
